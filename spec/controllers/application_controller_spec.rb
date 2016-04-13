16df6a11b26d0551eb9e1a1fa89f69340983ad69
Corinne Pingul <corinnepingul@gmail.com>
2016-04-13 15:37:05 -0700

    Adding some controller tests with VCR.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-CRIME_API_CALL = { cassette_name: "Crime_api_call" }
+CRIME_API_CALL = { cassette_name: "CRIME_API_CALL" }
 
 RSpec.describe ApplicationController, type: :controller do
   describe "loading homepage", vcr: CRIME_API_CALL do
@@ -10,6 +10,19 @@ RSpec.describe ApplicationController, type: :controller do
 
     it "should be successful" do
       expect(response).to be_ok
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template("index")
+    end
+
+    context "should returned the expected json object" do
+      it "has the right keys" do
+        data = JSON.parse(assigns(:data))
+        %w(event_clearance_subgroup incident_location).each do |key|
+          expect(data.map(&:keys).flatten.uniq).to include key
+        end
+      end
     end
   end
 end
